791e8b5b53a0e8fe06a41e6645641786dc753790
hyp3699 <heyuping369@outlook.com>
2026-03-15 18:56:17 +0800

    Python 使用启动脚本 启动多个文件

--- jiao/st.py
+++ jiao/st.py
@@ -5,7 +5,7 @@ import time
 subprocess.Popen(["python3", "a.py"])
 
 # 启动vps向Python容器推送运行信息脚本
-subprocess.Popen(["python3", "vps-Python.py"])
+subprocess.Popen(["python3", "Python.py"])
 
 print("所有程序已启动")
 while True:
