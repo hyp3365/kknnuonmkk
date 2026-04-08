f11a754ffb2f920d75930aa2c1b50cfaa7302798
hyp3699 <heyuping369@outlook.com>
2026-04-08 10:59:06 +0800

    Update sing-box.sh

--- jiao/sing-box.sh
+++ jiao/sing-box.sh
@@ -1269,7 +1269,7 @@ disable_open_sub() {
     skyblue "------------"
 	green "2. 停止gninx"
     skyblue "------------"
-	green "3."重启nginx"
+	green "3. 重启nginx"
     skyblue "------------"
     green "4. 关闭节点订阅"
     skyblue "------------"
@@ -1356,7 +1356,7 @@ disable_open_sub() {
             ;;
 
         6)
-            reading "请输入新的订阅端口[1-65535]:" sub_port
+            reading "请输入新的订阅端口(1-65535):" sub_port
             [ -z "$sub_port" ] && sub_port=$(shuf -i 2000-65000 -n 1)
             
             # 检查端口是否被占用
