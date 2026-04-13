6c3a56198765f24ed53cfe3073857fc181b4b8f2
hyp3699 <heyuping369@outlook.com>
2026-04-13 17:40:57 +0800

    Update sing-box08.sh

--- jiao/sing-box08.sh
+++ jiao/sing-box08.sh
@@ -718,15 +718,11 @@ get_info() {
   cat > ${work_dir}/url.txt <<EOF
 vless://${uuid}@${server_ip}:${vless_port}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.iij.ad.jp&fp=firefox&pbk=${public_key}&sid=${short_id}&type=tcp&headerType=none#${isp}_vless-reality
 
-
 vmess://$(echo "$VMESS"| base64 -w0)
-
 
 hysteria2://${uuid}@${server_ip}:${hy2_port}/?sni=www.bing.com&insecure=1&alpn=h3&obfs=none#${isp}_hysteria2
 
-
 tuic://${uuid}:${password}@${server_ip}:${tuic_port}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${isp}_tuic
-
 
 EOF
 echo ""
