648c50c15c2aded9743312b357afd1dd750688b1
hyp3699 <heyuping369@outlook.com>
2026-04-15 00:41:49 +0800

    Update sing-box08.sh

--- jiao/sing-box08.sh
+++ jiao/sing-box08.sh
@@ -2917,6 +2917,137 @@ iptables_ssl() {
     esac
 }
 
+# 其他
+vps_s() {
+    
+    clear
+    echo ""
+    green "=== 其他 ===\n"
+    green "1. 本机信息"
+    skyblue "-------------------"
+    green "2. 停止sing-box服务"
+    skyblue "-------------------"
+    green "3. 重启sing-box服务"
+    skyblue "-------------------"
+    purple "0. 返回主菜单"
+    skyblue "------------"
+    reading "\n请输入选择: " choice
+    case "${choice}" in
+        1)
+    clear
+    ip_address
+    
+    if [ "$(uname -m)" == "x86_64" ]; then
+      cpu_info=$(cat /proc/cpuinfo | grep 'model name' | uniq | sed -e 's/model name[[:space:]]*: //')
+    else
+      cpu_info=$(lscpu | grep 'Model name' | sed -e 's/Model name[[:space:]]*: //')
+    fi
+
+    cpu_usage=$(top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}')
+    cpu_usage_percent=$(printf "%.2f" "$cpu_usage")%
+
+    cpu_cores=$(nproc)
+
+    mem_info=$(free -b | awk 'NR==2{printf "%.2f/%.2f MB (%.2f%%)", $3/1024/1024, $2/1024/1024, $3*100/$2}')
+
+    disk_info=$(df -h | awk '$NF=="/"{printf "%d/%dGB (%s)", $3,$2,$5}')
+
+    country=$(curl -s ipinfo.io/country)
+    city=$(curl -s ipinfo.io/city)
+
+    isp_info=$(curl -s ipinfo.io/org)
+
+    cpu_arch=$(uname -m)
+
+    hostname=$(hostname)
+
+    kernel_version=$(uname -r)
+
+    congestion_algorithm=$(sysctl -n net.ipv4.tcp_congestion_control)
+    queue_algorithm=$(sysctl -n net.core.default_qdisc)
+
+    # 尝试使用 lsb_release 获取系统信息
+    os_info=$(lsb_release -ds 2>/dev/null)
+
+    # 如果 lsb_release 命令失败，则尝试其他方法
+    if [ -z "$os_info" ]; then
+      # 检查常见的发行文件
+      if [ -f "/etc/os-release" ]; then
+        os_info=$(source /etc/os-release && echo "$PRETTY_NAME")
+      elif [ -f "/etc/debian_version" ]; then
+        os_info="Debian $(cat /etc/debian_version)"
+      elif [ -f "/etc/redhat-release" ]; then
+        os_info=$(cat /etc/redhat-release)
+      else
+        os_info="Unknown"
+      fi
+    fi
+
+    clear
+    output=$(awk 'BEGIN { rx_total = 0; tx_total = 0 }
+        NR > 2 { rx_total += $2; tx_total += $10 }
+        END {
+            rx_units = "Bytes";
+            tx_units = "Bytes";
+            if (rx_total > 1024) { rx_total /= 1024; rx_units = "KB"; }
+            if (rx_total > 1024) { rx_total /= 1024; rx_units = "MB"; }
+            if (rx_total > 1024) { rx_total /= 1024; rx_units = "GB"; }
+
+            if (tx_total > 1024) { tx_total /= 1024; tx_units = "KB"; }
+            if (tx_total > 1024) { tx_total /= 1024; tx_units = "MB"; }
+            if (tx_total > 1024) { tx_total /= 1024; tx_units = "GB"; }
+
+            printf("总接收: %.2f %s\n总发送: %.2f %s\n", rx_total, rx_units, tx_total, tx_units);
+        }' /proc/net/dev)
+    current_time=$(date "+%Y-%m-%d %I:%M %p")
+    swap_used=$(free -m | awk 'NR==3{print $3}')
+    swap_total=$(free -m | awk 'NR==3{print $2}')
+
+    if [ "$swap_total" -eq 0 ]; then
+        swap_percentage=0
+    else
+        swap_percentage=$((swap_used * 100 / swap_total))
+    fi
+    swap_info="${swap_used}MB/${swap_total}MB (${swap_percentage}%)"
+    runtime=$(cat /proc/uptime | awk -F. '{run_days=int($1 / 86400);run_hours=int(($1 % 86400) / 3600);run_minutes=int(($1 % 3600) / 60); if (run_days > 0) printf("%d天 ", run_days); if (run_hours > 0) printf("%d时 ", run_hours); printf("%d分\n", run_minutes)}')
+    echo ""
+    echo -e "${white}系统信息详情${re}"
+    echo "------------------------"
+    echo -e "${white}主机名: ${purple}${hostname}${re}"
+    echo -e "${white}运营商: ${purple}${isp_info}${re}"
+    echo "------------------------"
+    echo -e "${white}系统版本: ${purple}${os_info}${re}"
+    echo -e "${white}Linux版本: ${purple}${kernel_version}${re}"
+    echo "------------------------"
+    echo -e "${white}CPU架构: ${purple}${cpu_arch}${re}"
+    echo -e "${white}CPU型号: ${purple}${cpu_info}${re}"
+    echo -e "${white}CPU核心数: ${purple}${cpu_cores}${re}"
+    echo "------------------------"
+    echo -e "${white}CPU占用: ${purple}${cpu_usage_percent}${re}"
+    echo -e "${white}物理内存: ${purple}${mem_info}${re}"
+    echo -e "${white}虚拟内存: ${purple}${swap_info}${re}"
+    echo -e "${white}硬盘占用: ${purple}${disk_info}${re}"
+    echo "------------------------"
+    echo -e "${purple}$output${re}"
+    echo "------------------------"
+    echo -e "${white}网络拥堵算法: ${purple}${congestion_algorithm} ${queue_algorithm}${re}"
+    echo "------------------------"
+    echo -e "${white}公网IPv4地址: ${purple}${ipv4_address}${re}"
+    echo -e "${white}公网IPv6地址: ${purple}${ipv6_address}${re}"
+    echo "------------------------"
+    echo -e "${white}地理位置: ${purple}${country} $city${re}"
+    echo -e "${white}系统时间: ${purple}${current_time}${re}"
+    echo "------------------------"
+    echo -e "${white}系统运行时长: ${purple}${runtime}${re}"
+    echo
+    ;;
+        2) stop_singbox ;;
+        3) restart_singbox ;;
+        0) menu ;;
+        *) red "无效的选项！" && sleep 1 && manage_singbox;;
+    esac
+}            
+
 # singbox 管理
 manage_singbox() {
     # 检查sing-box状态
@@ -3214,10 +3345,11 @@ menu() {
    purple "11. 更新脚本"
    purple "12. VPS秘钥登录配置"
    purple "13. iptables管理"
+   purple "14. 其他"
    echo  "==============="
    red "0. 退出脚本"
    echo "==========="
-   reading "请输入选择(0-13): " choice
+   reading "请输入选择(0-14): " choice
    echo ""
 }
 
@@ -3268,8 +3400,9 @@ while true; do
 		11) update_script ;;
 		12) vps_ssl ;;
 		13) iptables_ssl ;;
+		14) vps_s ;;
         0) exit 0 ;;
-        *) red "无效的选项，请输入 0 到 13" ;;
+        *) red "无效的选项，请输入 0 到 14" ;;
    esac
    read -n 1 -s -r -p $'\033[1;91m按任意键返回...\033[0m'
 done
