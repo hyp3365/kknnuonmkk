08aec9246969a44ed5b6aadf7361fa2e270d5c30
hyp3699 <heyuping369@outlook.com>
2026-07-28 21:57:21 +0800

    Update sing-box00008.sh

--- jiao/sing-box00008.sh
+++ jiao/sing-box00008.sh
@@ -683,7 +683,7 @@ EOF
 
 # 创建快捷指令（自动下载脚本到本地保存）
 create_shortcut() {
-    local remote_url="https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/sing-box08.sh"
+    local remote_url="https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/sing-box00008.sh"
     local local_file="$work_dir/sb.sh"
     if [ ! -s "$local_file" ]; then
         mkdir -p "$work_dir"
