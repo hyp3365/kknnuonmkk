f631294d9569e5f7c54aef64a51e760a22c0374d
hyp3699 <heyuping369@outlook.com>
2026-08-01 10:32:36 +0800

    Update sing-box00008.sh

--- jiao/sing-box00008.sh
+++ jiao/sing-box00008.sh
@@ -4030,7 +4030,7 @@ menu() {
    green "Telegram群组: ${purple}https://t.me/eooceu${re}"
    green "Github地址: ${purple}https://github.com/eooce/sing-box${re}\n"
    green "${purple}快捷命令sb或者b${re}"
-   purple "=== 老王sing-box四合一安装脚本 a===\n"
+   purple "=== 老王sing-box四合一安装脚本 0.1===\n"
    printf "${purple}---Argo 状态: %s${re}\n" "$(to_chinese "$argo_status")"
    printf "${purple}--Nginx 状态: %s${re}\n" "$(to_chinese "$nginx_status")"
    printf "${purple}singbox 状态: %s${re}\n\n" "$(to_chinese "$singbox_status")"
