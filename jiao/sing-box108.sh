eeed26b91d42e45fa24fa207eb5c9fbf1acc6262
hyp3699 <heyuping369@outlook.com>
2026-08-17 17:22:33 +0800

    Update sing-box108.sh

--- jiao/sing-box108.sh
+++ jiao/sing-box108.sh
@@ -5142,8 +5142,26 @@ warp_manage() {
                 green "已卸载"
                 sleep 1
                 ;;
-			3)
-                local state port pw ip
+			3) show_info; pause ;;
+            0)
+                warp_manage
+                return
+                ;;
+            *)
+                red "无效选项"
+                sleep 1
+                ;;
+        esac
+        warp_manage
+        ;;
+        0)  menu ;;
+        00) exit 0 ;;
+        *)  red "无效选项"; sleep 1; warp_manage ;;
+    esac
+}
+
+show_info() {
+local state port pw ip
                 if pgrep -f "singbox_web.py" >/dev/null 2>&1; then
                     state="running"
                 else
@@ -5170,24 +5188,7 @@ warp_manage() {
                 echo "  管理地址: http://${ip}:${port}/"
                 echo "  访问口令: ${pw}"
                 echo
-            
-                ;;
-            0)
-                warp_manage
-                return
-                ;;
-            *)
-                red "无效选项"
-                sleep 1
-                ;;
-        esac
-        warp_manage
-        ;;
-        0)  menu ;;
-        00) exit 0 ;;
-        *)  red "无效选项"; sleep 1; warp_manage ;;
-    esac
-}
+           }
 
 #把fanout socks出站添加到sing-box出站
 extract_fanout_socks() {
