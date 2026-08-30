1d417315bcc21febc8acb9ab08b68fbc51c84fc8
hyp3699 <heyuping369@outlook.com>
2026-08-30 23:37:30 +0800

    Update sing-box108.sh

--- jiao/sing-box108.sh
+++ jiao/sing-box108.sh
@@ -7242,38 +7242,32 @@ ArgoDomain=$get_argodomain
 change_argo_domain() {
     generate_vars
     content=$(cat "$client_dir")
-    vmess_url=$(grep -o 'vmess://[^ ]*' "$client_dir" | head -n1)
+    vmess_url=$(grep -o 'vmess://[^ ]*' "$client_dir")
     vmess_prefix="vmess://"
     encoded_vmess="${vmess_url#"$vmess_prefix"}"
     decoded_vmess=$(echo "$encoded_vmess" | base64 --decode)
     updated_vmess=$(echo "$decoded_vmess" | jq --arg new_domain "$ArgoDomain" '.host = $new_domain | .sni = $new_domain')
     encoded_updated_vmess=$(echo "$updated_vmess" | base64 | tr -d '\n')
     new_vmess_url="${vmess_prefix}${encoded_updated_vmess}"
-    new_content=$(printf '%s\n' "$content" | sed "s|$vmess_url|$new_vmess_url|")
-
-    # VLESS
-    vless_uuid=$(jq -r '.inbounds[] | select(.type=="vless") | .users[0].uuid // empty' /etc/sing-box/conf/inbounds.json)
-    node_remark="${isp}_vless-ws-argo"
-    vless_url="vless://${vless_uuid}@${CFIP:-'cf.877774.xyz'}:443?encryption=none&security=tls&type=ws&host=${ArgoDomain}&sni=${ArgoDomain}&path=%2Fasasbsbs-vless#${node_remark}"
-
-    # Trojan
-    trojan_password=$(jq -r '.inbounds[] | select(.type=="trojan") | .users[0].password // empty' /etc/sing-box/conf/inbounds.json)
-    node_remark="${isp}_trojan-ws-argo"
-    trojan_url="trojan://${trojan_password}@${CFIP:-'cf.877774.xyz'}:443?security=tls&type=ws&host=${ArgoDomain}&sni=${ArgoDomain}&path=%2Fasasbsbs-trojan#${node_remark}"
-
-    
-    new_content="${new_content}
-${vless_url}
-
-${trojan_url}
-
-"
-
+    new_content=$(echo "$content" | sed "s|$vmess_url|$new_vmess_url|")
     echo "$new_content" > "$client_dir"
+
+    # 获取 VLESS 节点
+    vless_uuid=$(jq -r '.inbounds[] | select(.type=="vless") | .users[0].uuid // empty' /etc/sing-box/conf/inbounds.json)
+    node_remark_vless="${isp}_vless-ws-argo"
+    vless_url="vless://${vless_uuid}@${CFIP:-'cf.877774.xyz'}:443?encryption=none&security=tls&type=ws&host=${ArgoDomain}&sni=${ArgoDomain}&path=%2Fasasbsbs-vless#${node_remark_vless}"
+
+    # 获取 Trojan 节点
+    trojan_password=$(jq -r '.inbounds[] | select(.type=="trojan") | .users[0].password // empty' /etc/sing-box/conf/inbounds.json)
+    node_remark_trojan="${isp}_trojan-ws-argo"
+    trojan_url="trojan://${trojan_password}@${CFIP:-'cf.877774.xyz'}:443?security=tls&type=ws&host=${ArgoDomain}&sni=${ArgoDomain}&path=%2Fasasbsbs-trojan#${node_remark_trojan}"
+
+    # 在文件末尾追加：中间隔一个空行，末尾增加一个空行
+    printf "\n%s\n\n%s\n\n" "$vless_url" "$trojan_url" >> "$client_dir"
 
     base64 -w0 "${work_dir}/url.txt" > "${work_dir}/sub.txt"
 
-    green "Argo节点已更新，更新订阅或手动复制节点"
+    green "Argo节点已更新，更新订阅或手动复制节点："
 }
 
 # 查看节点信息和订阅链接
