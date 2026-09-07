d04718b5fff14f66e40ce08aab75cfd101d8228b
hyp3699 <heyuping369@outlook.com>
2026-09-07 18:44:01 +0800

    Update sing-box-cf.sh

--- jiao/sing-box-cf.sh
+++ jiao/sing-box-cf.sh
@@ -3065,7 +3065,7 @@ EOF
       {"tag":"telegram","type":"remote","format":"binary","url":"https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/sing/geo-lite/geosite/telegram.srs"},
       {"tag":"youtube","type":"remote","format":"binary","url":"https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/sing/geo-lite/geosite/youtube.srs"}
     ],
-    "rules": [{"rule_set": []}],
+    "rules": [],
     "final": "direct"
   }
 }
