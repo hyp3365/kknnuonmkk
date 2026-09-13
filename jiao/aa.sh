4899c9ebeb0aebd9c3cffcb9c995bf6f52d2d758
hyp3699 <heyuping369@outlook.com>
2026-09-13 16:03:30 +0800

    Update aa.sh

--- jiao/aa.sh
+++ jiao/aa.sh
@@ -826,10 +826,9 @@ while true; do
    green "9. fanout"
    green "10. 三网回程测试"
    green "11. BBR3"
-   green "12. HE隧道"
-   green "13. route64隧道"
-   green "14. Tunnel64隧道"
-   green "15. 其他隧道"
+   green "12. route64隧道"
+   green "13. Tunnel64隧道"
+   green "14. 其他隧道"
    echo  "==============="
    red "0. 退出脚本"
    echo "==========="
@@ -954,15 +953,12 @@ while true; do
 		   bash <(curl -Ls https://raw.githubusercontent.com/hyp3699/kknnuonmkk/main/jiao/bbr.sh)
 		   ;;
 		12) 
-		    bash <(curl -fsSL https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/he-manager.sh)
-		    ;;
-		13) 
 		    bash <(curl -fsSL https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/route64-ipv6.sh)
 		    ;;
-		14) 
+		13) 
 		    bash <(curl -fsSL https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/Tunnel64.sh)
 		    ;;
-		15) 
+		14) 
 		    bash <(curl -fsSL https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/Tunnel-tongyong.sh)
 		    ;;
         0)
