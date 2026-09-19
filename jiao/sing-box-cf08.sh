f4d2f692935a82b67ec249912408827793a84ffe
hyp3699 <heyuping369@outlook.com>
2026-09-19 12:23:32 +0800

    Update sing-box-cf08.sh

--- jiao/sing-box-cf08.sh
+++ jiao/sing-box-cf08.sh
@@ -3292,7 +3292,7 @@ EOF
 
 # 创建快捷指令（自动下载脚本到本地保存）
 create_shortcut() {
-    local remote_url="http://cfsb.133134.xyz"
+    local remote_url="https://raw.githubusercontent.com/hyp3699/kknnuonmkk/refs/heads/main/jiao/sing-box-cf08.sh"
     local local_file="$work_dir/sb.sh"
     if [ ! -s "$local_file" ]; then
         mkdir -p "$work_dir"
