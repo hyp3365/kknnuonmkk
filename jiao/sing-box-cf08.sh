c8b6596a5b12b5e6c56f3ea37af6013d42024958
hyp3699 <heyuping369@outlook.com>
2026-09-19 20:36:07 +0800

    Update sing-box-cf08.sh

--- jiao/sing-box-cf08.sh
+++ jiao/sing-box-cf08.sh
@@ -6075,8 +6075,8 @@ except:
 units = ["B", "KB", "MB", "GB", "TB", "PB"]
 i = 0
 v = float(n)
-while v >= 1024 and i < len(units) - 1:
-    v /= 1024
+while v >= 1000 and i < len(units) - 1:
+    v /= 1000
     i += 1
 if i == 0:
     print(f"{int(v)} {units[i]}")
